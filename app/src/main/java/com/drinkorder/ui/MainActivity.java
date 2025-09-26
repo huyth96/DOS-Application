0cd65e579e63864e5f663658e2ba1fe03549069f
Huy <Huyth.work@gmail.com>
2025-09-26 12:55:13 +0700

    Thêm google map

--- app/src/main/java/com/drinkorder/ui/MainActivity.java
+++ app/src/main/java/com/drinkorder/ui/MainActivity.java
@@ -1,4 +1,6 @@
 package com.drinkorder.ui;
+
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -6,22 +8,44 @@ import com.drinkorder.R;
 import com.drinkorder.ui.cart.CartFragment;
 import com.drinkorder.ui.home.HomeFragment;
 import com.drinkorder.ui.orders.OrdersFragment;
+import com.drinkorder.ui.map.MapActivity;   // 👈 import MapActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
   @Override protected void onCreate(Bundle b){
     super.onCreate(b);
     setContentView(R.layout.activity_main);
+
     BottomNavigationView nav = findViewById(R.id.bottomNav);
     nav.setOnItemSelectedListener(item -> {
-      Fragment f;
-      int id=item.getItemId();
-      if (id==R.id.tab_cart) f=new CartFragment();
-      else if (id==R.id.tab_orders) f=new OrdersFragment();
-      else f=new HomeFragment();
-      getSupportFragmentManager().beginTransaction().replace(R.id.container,f).commit();
-      return true;
+      int id = item.getItemId();
+
+      if (id == R.id.tab_cart) {
+        replaceFragment(new CartFragment());
+        return true;
+
+      } else if (id == R.id.tab_orders) {
+        replaceFragment(new OrdersFragment());
+        return true;
+
+      } else if (id == R.id.tab_map) {
+        // 👉 Trường hợp Map: mở Activity mới
+        startActivity(new Intent(this, MapActivity.class));
+        return false; // hoặc true: tuỳ bạn có muốn giữ trạng thái chọn Map hay không
+
+      } else {
+        replaceFragment(new HomeFragment());
+        return true;
+      }
     });
+
     nav.setSelectedItemId(R.id.tab_home);
+  }
+
+  private void replaceFragment(Fragment f) {
+    getSupportFragmentManager()
+            .beginTransaction()
+            .replace(R.id.container, f)
+            .commit();
   }
 }
